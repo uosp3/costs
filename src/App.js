66bd4420a5d59a250a55112c99728afbb58d456f
Edson Gomes dos Santos <110746401+uosp3@users.noreply.github.com>
2023-02-12 18:31:32 -0300

    carga

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
+
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+function App() {
+  return (
+    <Router>
+      <Navbar></Navbar>
+      <Container customClass='min-height'>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/projects" element={<Projects/>}></Route>
+            <Route path="/company" element={<Company/>}></Route>
+            <Route path="/contact" element={<Contact/>}></Route>
+            <Route path="/newproject" element={<NewProject/>}></Route>
+            <Route path="/project/:id" element={<Project/>}></Route>
+          </Routes>
+      </Container>
+      <Footer></Footer>
+    </Router>
+  );
+}
+
+export default App;
+//npm run backend
